e7161911e0c140a2a44001aba490040ca0409632
ugeorge <ugeorge@2f3b4d84-7048-4793-a15c-83de0941b73a>
2013-01-28 13:29:16 +0000

    Release 0.1 - ghb

---
+++ f2cc/tags/0.1/source/exceptions/unknownarraysizeexception.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2011-2012 Gabriel Hjort Blindell <ghb@kth.se>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THIS SOFTWARE NOR THE
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef F2CC_SOURCE_EXCEPTIONS_UNKNOWNARRAYSIZEEXCEPTION_H_
+#define F2CC_SOURCE_EXCEPTIONS_UNKNOWNARRAYSIZEEXCEPTION_H_
+
+/**
+ * @file
+ * @author  Gabriel Hjort Blindell <ghb@kth.se>
+ * @version 0.1
+ *
+ * @brief Defines exception for when trying to use an array size when it is
+ *        unknown.
+ */
+
+#include "exception.h"
+#include <string>
+
+namespace f2cc {
+
+/**
+ * @brief Used when trying to use an array size when it is unknown.
+ */
+class UnknownArraySizeException : public Exception {
+  public:
+    /**
+     * @copydoc Exception::Exception(const std::string&, int, const std::string&)
+     */
+    UnknownArraySizeException(const std::string& source_file, int source_line,
+                          const std::string& message) throw();
+
+    /**
+     * @copydoc Exception::~Exception()
+     */
+    virtual ~UnknownArraySizeException() throw();
+
+  protected:
+    /**
+     * @copydoc Exception::type()
+     */
+    virtual std::string type() const throw();
+};
+
+}
+
+#endif
---
+++ f2cc/tags/0.1/source/forsyde/unzipxsy.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2011-2012 Gabriel Hjort Blindell <ghb@kth.se>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THIS SOFTWARE NOR THE
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "unzipxsy.h"
+#include <typeinfo>
+
+using namespace f2cc::Forsyde;
+using std::string;
+using std::bad_cast;
+
+UnzipxSY::UnzipxSY(const Id& id) throw()
+        : Process(id) {}
+
+UnzipxSY::~UnzipxSY() throw() {}
+
+bool UnzipxSY::operator==(const Process& rhs) const throw() {
+    if (Process::operator==(rhs)) return false;
+
+    try {
+        dynamic_cast<const UnzipxSY&>(rhs);
+    }
+    catch (bad_cast&) {
+        return false;
+    }
+    return true;
+}
+
+string UnzipxSY::type() const throw() {
+    return "unzipxSY";
+}
+
+void UnzipxSY::moreChecks() throw(InvalidProcessException) {
+    if (getInPorts().size() != 1) {
+        THROW_EXCEPTION(InvalidProcessException, string("Process \"")
+                        + getId()->getString() + "\" of type \""
+                        + type() + "\" must have exactly one (1) in port");
+    }
+}
