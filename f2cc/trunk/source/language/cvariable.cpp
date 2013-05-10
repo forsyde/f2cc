641b4e0e1e33d24bead85fc2cdf5c568a6d5ac1a
ugeorge <ugeorge@2f3b4d84-7048-4793-a15c-83de0941b73a>
2013-05-10 18:34:00 +0000

    second part of merging branch with trunk

--- f2cc/trunk/source/language/cvariable.cpp
+++ f2cc/trunk/source/language/cvariable.cpp
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2011-2012 Gabriel Hjort Blindell <ghb@kth.se>
+ * Copyright (c) 2011-2013
+ *     Gabriel Hjort Blindell <ghb@kth.se>
+ *     George Ungureanu <ugeorge@kth.se>
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
@@ -35,6 +37,10 @@ CVariable::CVariable(const std::string& name, const CDataType& type)
         throw(InvalidArgumentException) : name_(name), type_(type) {}
 
 CVariable::~CVariable() throw() {}
+
+void CVariable::changeReferenceString(std::string name) throw(){
+	name_ = name;
+}
 
 CDataType* CVariable::getDataType() throw() {
     return &type_;
